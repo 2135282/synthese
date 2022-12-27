73c864c419fda8e8a10e19b2e1108fe1e48ebec7
Joa-Lee <2135282@cegepoutaouais.qc.ca>
2022-12-26 23:38:58 -0500

    Class Articles

--- synthese/Class/Articles.cs
+++ synthese/Class/Articles.cs
@@ -12,7 +12,6 @@ namespace synthese.Class
     internal class Articles
     {
         //Attributs
-        public List<Articles> listeArticles;    //  la liste des articles
         private SqlConnection connection;
         private SqlCommand command;
         private SqlDataReader reader;
@@ -23,30 +22,30 @@ namespace synthese.Class
 
 
         //Propriétés
-        public List<Articles> ListeArticles
-        {
-            get { return this.listeArticles; }
-            set { this.listeArticles = value;}
-        }
 
         public SqlConnection Connection { get => connection; set => connection = value; }
         public SqlCommand Command { get => command; set => command = value; }
         public SqlDataReader Reader { get => reader; set => reader = value; }
         public SqlDataAdapter Adapter { get => adapter; set => adapter = value; }
         public string ConnectionString { get => connectionString; }     // Pas de set afin de bloquer la possibilité de la modifier
-        public DataTable DtEtudiants { get => dtProjetSynthese; set => dtProjetSynthese = value; }
+        public DataTable DtProjetSynthese { get => dtProjetSynthese; set => dtProjetSynthese = value; }
         public DataSet DsArticles { get => dsArticles; set => dsArticles = value; }
 
         //Constructeur par paramètres
         public Articles()
         {
-                // Integrated Security = True;
-                connectionString = "Data Source=1105R1PC1;Initial Catalog=ProjetSynthese;User ID=sa";
-                connection = new SqlConnection(connectionString);
-                command = new SqlCommand();
-                adapter = new SqlDataAdapter();
-                dsArticles = new DataSet();
-                dtProjetSynthese = new DataTable();
+            // Integrated Security = True;
+            connectionString = "Data Source=1105R1PC1;Initial Catalog=ProjetSynthese;User ID=sa";
+            connection = new SqlConnection(connectionString);
+            command = new SqlCommand();
+            adapter = new SqlDataAdapter();
+            dsArticles = new DataSet();
+            dtProjetSynthese = new DataTable();
         }
+
+        // ** Exercices Interface_ClassesMeres avec les animaux **
+        // Méthode abstraite Consommer().
+        // Cette méthode doit être surchagée (Override) dans les classes filles. 
+        public abstract void Consommer();
     }
 }
